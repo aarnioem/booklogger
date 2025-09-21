9170b0f289e7c563f2912c5ba3ddb952aae7a700
aarnioem <aarnioem@gmail.com>
2025-09-21 20:58:43 +0300

    queries no longer use *

--- logs.py
+++ logs.py
@@ -5,12 +5,12 @@ def add_log(title, author, status, rating, review, user_id):
     db.execute(sql, [user_id, title, author, status, rating, review])
 
 def get_logs_by_user_id(user_id):
-    sql = "SELECT * FROM books WHERE user_id = ?"
+    sql = "SELECT id, user_id, title, author, status, rating, review FROM books WHERE user_id = ?"
     logs = db.query(sql, [user_id])
     return logs
 
 def get_log_by_id(log_id):
-    sql = "SELECT * FROM books WHERE id = ?"
+    sql = "SELECT id, user_id, title, author, status, rating, review FROM books WHERE id = ?"
     log = db.query(sql, [log_id])
     return log[0]
 
